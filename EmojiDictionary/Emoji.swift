8c5d8a0040bb07c31062a3d71ad9fd078e58f922
Liam Davidson <ldavidson@lcs.on.ca>
2019-11-13 21:01:22 -0500

    Defined the new Emoji struct (this is the model)

---
+++ EmojiDictionary/Emoji.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji.swift
+//  EmojiDictionary
+//
+//  Created by Davidson, Liam on 2019-11-13.
+//  Copyright © 2019 Davidson, Liam. All rights reserved.
+//
+
+import Foundation
+
+//define Emoji struct
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
